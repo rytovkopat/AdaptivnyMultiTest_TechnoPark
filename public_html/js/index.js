901dec4012cbd8a03d608ef5fea425fe2208b47d
rytovkopat <gzintzfirmltd@mail.ru>
2016-03-17 00:39:12 +0300

    Clearance before update

--- public_html/js/index.js
+++
@@ -1,6 +0,0 @@
-define( //запускает контроль адресной строки для хеш-навигации
-    ['backbone', 'router'],
-    function (Backbone, router) {
-        Backbone.history.start();
-    }
-);
--- public_html/js/nav_event.js
+++
@@ -1,7 +0,0 @@
-define( //!запускает backbone event, который позже уйдет на trigger роута по кнопкам
-	//смотри BaseView и initialise в router
-    ['underscore','backbone'] ,
-    function (_, Backbone) {
-        return _.extend({}, Backbone.Events);
-    }
-);
